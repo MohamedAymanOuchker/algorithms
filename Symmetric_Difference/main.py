38bc47596b4a7546239a9bf345957339c73258c7
Mohamed Ayman Ouchker <50711522+MohamedAymanOuchker@users.noreply.github.com>
2021-03-01 13:55:57 +0100

    Update main.py

--- Symmetric_Difference/main.py
+++ Symmetric_Difference/main.py
@@ -1,6 +1,28 @@
 # Find the Symmetric Difference
 
-def sym(args, args2):
-    return args.symmetric_difference(args2)
+def sym(args1, args2):
+    return args1.symmetric_difference(args2)
 
-print(sym({1,2,3},{2,3,4}))
+i = ''
+j = ''
+args1 = {''}
+args2 = {''}
+
+while i != 'q':
+    i = input("Add element to set 1 (type 'q' to quit): ")
+    if i == 'q':
+        break
+    else:
+        args1.add(i)
+
+while j != 'q':
+    j = input("Add element to set 2 (type 'q' to quit): ")
+    if j == 'q':
+        break
+    else:
+        args2.add(j)
+
+args1.remove('')
+args2.remove('')
+
+print('\nThe symmetric difference: ', sym(args1, args2))
