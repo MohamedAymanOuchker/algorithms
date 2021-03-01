3dedd191779ffd3061852283c72c3f96b1379b8e
Mohamed Ayman Ouchker <50711522+MohamedAymanOuchker@users.noreply.github.com>
2021-03-01 14:17:29 +0100

    Update main.c

--- Symmetric_Difference/main.c
+++ Symmetric_Difference/main.c
@@ -1,39 +1,92 @@
-#include <stdio.h>
-#include <string.h>
- 
-const char *A[] = { "1", "2", "3" };
-const char *B[] = { "2", "3", "4" };
- 
-#define LEN(x) sizeof(x)/sizeof(x[0])
- 
-int in_set(const char *const x[], int len, const char *match)
-{
-	int i;
-	for (i = 0; i < len; i++)
-		if (x[i] && !strcmp(x[i], match))
-			return 1;
-	return 0;
-}
- 
-/* x - y */
-void show_diff(const char *const x[], int lenx, const char *const y[], int leny)
-{
-	int i;
-	for (i = 0; i < lenx; i++)
-		if (x[i] && !in_set(y, leny, x[i]))
-			printf("  %s\n", x[i]);
-}
- 
-/* X ^ Y */
-void show_sym_diff(const char *const x[], int lenx, const char *const y[], int leny)
-{
-	show_diff(x, lenx, y, leny);
-	show_diff(y, leny, x, lenx);
-}
- 
+// Find the Symmetric Difference
+
+#include<stdio.h>
 int main()
 {
-	printf("\nA ^ B:\n");  show_sym_diff(A, LEN(A), B, LEN(B));
- 
-	return 0;
+    int a[10],b[10],c[10],d[10],m=0,k=0,n=0,n1,n2,l,i,j,sy[100];
+    printf("Enter size of set A: ");
+    scanf("%d",&n1);
+    printf("Enter element of set: ");
+    for( i=0;i<n1;i++)
+    scanf("%d",&a[i]);
+    printf("Enter size of set B: ");
+    scanf("%d",&n2);
+    printf("Enter element of set: ");
+    for( i=0;i<n2;i++)
+    scanf("%d",&b[i]);
+   
+    
+    // logic for find A-B
+    
+    for( i=0;i<n1;i++)
+    {
+       // here we check that is b[i] already present in the set
+      // if present then ignore it otherwise add it to the set
+        for(j=0;j<n2;j++)
+        {
+            if(b[j]==a[i])
+             break;
+        }
+        if(j==n2)
+        {
+            
+            for(l=0;l<k;l++)
+            {
+                if(c[l]==a[i])
+                 break;
+            }
+            if(l==k)
+            {
+                c[k]=a[i];
+                k++;
+            }
+        }
+        
+    }
+    
+    // logic for find B-A
+    
+    for( i=0;i<n2;i++)
+    {
+        for(j=0;j<n1;j++)
+        {
+            if(b[i]==a[j])
+             break;
+        }
+        if(j==n1)
+        {
+          // here we check that is b[i] already present in the set
+          // if present then ignore it otherwise add it to the set 
+            for(l=0;l<m;l++)
+            {
+                if(d[l]==b[i])
+                 break;
+            }
+            if(l==m)
+            {
+                d[m]=b[i];
+                m++;
+            }
+        }
+        
+    }
+    
+    //logic for symmetric Difference
+    
+    for(i=0;i<k;i++)
+    {
+        sy[n]=c[i];
+        n++;
+    }
+    for(i=0;i<m;i++)
+    {
+        sy[n]=d[i];
+        n++;
+    }
+    
+    printf("\nsymmetric Difference of sets is:\n");
+    for(i=0;i<n;i++)
+     printf("%d ",sy[i]);
+    return 0;
+    
 }
